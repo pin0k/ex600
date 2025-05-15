522ece6aee00a86f825cc1e951f1b8cf1e5c7adf
Анна <pin0k@yandex.ru>
2025-05-15 04:26:15 +0300

    ex2_600

--- local/php_interface/events/lang/ru/ex2_600.php
+++ local/php_interface/events/lang/ru/ex2_600.php
@@ -1,3 +1,3 @@
 <?php
-$MESS['EMAIL'] = 'test100500@academy.1c-bitrix.ru';
-$MESS['EMAIL_TO'] = 'test100500@academy.1c-bitrix.ru';
+$MESS['EMAIL'] = 'test100500@a';
+$MESS['EMAIL_TO'] = 'test100500@';
